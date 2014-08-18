2a0c9c835b6d3ed4f2b478d908e4c5664558e28f
Myles Borins <myles@famo.us>
2014-08-18 13:33:09 -0700

    Break out server to be hapi plugin

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 'use strict';
 var Hapi = require('hapi');
-var path = require('path');
 
 var server = Hapi.createServer('localhost', Number(process.argv[2]) || 1337);
 
-server.route({
-  path: '/{param*}',
-  method: 'GET',
-  handler: {
-    directory: { 
-      path: path.join(__dirname, 'public/')
-    }
-  }
+server.pack.register(require('./plugin'), function (err) {
+  if (err) { console.error('Failes to load a plugin:', err); }
 });
 
 server.start();
