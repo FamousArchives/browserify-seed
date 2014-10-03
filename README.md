a0b7f2d742f34ccd8145d7ef9490fbc9690e55d4
Hongxu Liu <djshu.us@gmail.com>
2014-10-02 20:06:44 -0700

    Logging server port and updated readme

--- README.md
+++ README.md
@@ -19,6 +19,8 @@ npm i # install dependencies
 ###Development
 Run the dev server with ```npm run start-dev```
 
+Now the dev server should run on localhost:1337
+
 Run the linters with ```npm run-script lint```
 
 Run All Tests with ```npm test```
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 var Hapi = require('hapi');
 
-var server = Hapi.createServer('localhost', Number(process.argv[2]) || 1337);
+var port = Number(process.argv[2]) || 1337;
+var server = Hapi.createServer('localhost', port);
 
 server.pack.register(require('./plugin'), function (err) {
   if (err) { console.error('Failes to load a plugin:', err); }
 });
 
 server.start();
+console.log('server started on port', port);
